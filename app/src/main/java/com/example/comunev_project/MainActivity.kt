0cbe4ea56b1f5ab31f71503333392e16c090b826
Saumi-Pandya <saumipandya@gmail.com>
2021-06-02 12:39:17 +0530

    Handled errors and exceptions

--- app/src/main/java/com/example/comunev_project/MainActivity.kt
+++ app/src/main/java/com/example/comunev_project/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
 import org.json.JSONArray
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -19,58 +22,79 @@ import org.json.JSONObject
 class MainActivity : AppCompatActivity() {
 
 
-
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //View binding
+        val pbar = findViewById<ProgressBar>(R.id.pbar)
         val parse_data_button = findViewById<Button>(R.id.parse_json)
+        val errorview = findViewById<TextView>(R.id.errorview)
+        errorview.text = "Data will be displayed here in a recycler view"
 
+        pbar.isVisible=false;
 
+        //setting up onClick listener
         parse_data_button
-            .setOnClickListener{
+            .setOnClickListener {
+                errorview.isVisible=false
+                pbar.isVisible=true
                 parseJson()
             }
     }
 
+    
     @RequiresApi(Build.VERSION_CODES.KITKAT)
-    fun parseJson(){
-
-
+    fun parseJson() {
         val users = mutableListOf<User>()
+        val errorview = findViewById<TextView>(R.id.errorview)
+        val pbar = findViewById<ProgressBar>(R.id.pbar)
+
+
+
         lifecycleScope.launch {
             val googleData = withContext(Dispatchers.IO) {
                 getDataFromNetwork("https://randomuser.me/api/?results=100&inc=name")
             }
 
-            val jsonObj = JSONObject(googleData)
-            val jsonArray = jsonObj.getJSONArray("results")
+            pbar.isVisible=false
 
-            for (i in 0 until jsonArray.length()) {
-                val user = parseUser(jsonArray.getJSONObject(i))
-                users.add(user)
+            if (googleData == "No Internet Connection") {
+                Toast.makeText(this@MainActivity,"$googleData",Toast.LENGTH_SHORT).show()
+
+            }
+            else {
+                val jsonObj = JSONObject(googleData)
+                val jsonArray = jsonObj.getJSONArray("results")
+
+                for (i in 0 until jsonArray.length()) {
+                    val user = parseUser(jsonArray.getJSONObject(i))
+                    users.add(user)
+                }
+                pbar.isEnabled=false
+                setRecyclerView(users)
             }
 
-            setRecyclerView(users)
 
-           
         }
     }
 
-    private fun parseUser(jsonObject: JSONObject): User{
-            val userObj = jsonObject.getJSONObject("name")
-            val user = User(userObj.getString("title"),userObj.getString("first"),userObj.getString("last"))
+    private fun parseUser(jsonObject: JSONObject): User {
+        val userObj = jsonObject.getJSONObject("name")
+        val user =
+            User(userObj.getString("title"), userObj.getString("first"), userObj.getString("last"))
 
-            return user
+        return user
     }
 
-    private fun setRecyclerView(nameList:List<User>){
+    private fun setRecyclerView(nameList: List<User>) {
         val recyView = findViewById<RecyclerView>(R.id.recy_view)
         recyView.apply {
-            layoutManager= LinearLayoutManager(this@MainActivity)
+            layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = name_adapter(nameList)
             setHasFixedSize(true)
         }
-        recyView.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
+        recyView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 }
